8669509e12a53bf2322bd7a585bfd5132e238112
Mickey Burks <mburks@uber.com>
2019-09-11 15:48:21 -0700

    Fix some issues

--- bin.js
+++ bin.js
@@ -1,14 +1,20 @@
 
-import serve from './index.js';
 import {execFile} from 'child_process';
 import {promisify} from 'util';
+
+// might be from using in conjunction with @micburks/jspm-loader
+import serve from '../@micburks/serve/index.js';
 
 const run = promisify(execFile);
 const command = process.argv.slice(2);
 
 async function callback(req) {
   try {
-    const {stdout} = await run('node', command);
+    const {stdout} = await run(
+      'node',
+      ['--experimental-modules', '--loader', '@micburks/jspm-loader', ...command],
+      {cwd: process.cwd()}
+    );
     return stdout;
   } catch (e) {
     console.error(e);
